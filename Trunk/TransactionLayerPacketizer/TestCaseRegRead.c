31707a0f63aa894f1d82ed29588cc9586a0a2133
Full Name <jozsab1@gmail.com>
2015-03-24 20:16:14 +0200

    - darn early commits

--- Trunk/TransactionLayerPacketizer/TestCaseRegRead.c
+++ Trunk/TransactionLayerPacketizer/TestCaseRegRead.c
@@ -3,9 +3,14 @@
 void TestCaseReadRegisterValue()
 {
 	struct TransactionLayerPacket *PacketQueueStore;
+
+	//init the queue system for a new testcase
 	InitPacketQueueForNewTestCase();
 
+	//get a packet store from queue
 	PacketQueueStore = GetPacketQueueStore();
+	//fill the packet with what we need
 	BuildPcktCCMD( PacketQueueStore->Packet, &PacketQueueStore->PacketSize, TRL_RW_Read, RA_Configuration, NULL, CCMD_PL_NONE );
+	//queue the packet to be sent to the device
 	QueuePacket( PacketQueueStore );
 }
